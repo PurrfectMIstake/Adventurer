4e99d922d67d37ada33620a0acf74b4caadd12b1
iamcion <iamcion@hotmail.com>
2013-11-26 08:55:09 +1000

    V 1.1.3 Update.
    Updated Events.java

--- src/me/iamcion/adventurer/Events.java
+++ src/me/iamcion/adventurer/Events.java
@@ -4,6 +4,7 @@ import java.util.List;
  
 
 
+
 import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -16,25 +17,32 @@ public class Events implements Listener{
         Main plugin;
         //1.1 - Adding Config
         public void loadConfiguration(){
- 		   }
+                    }
         //this is called a constructor: this is called whenever the class is initialized, so on "new Event()"
         public Events(Main plugin) {
                 this.plugin = plugin;
         }
-      
-        @EventHandler(priority = EventPriority.NORMAL)
-         public void onBlockBreak(BlockBreakEvent event){
-                Player player = event.getPlayer();
-                if(!player.isOp()){
-                player.damage(600*300*300D);
-                 player.sendMessage(ChatColor.DARK_RED + "Dont cheat! Play properly");
- 
-         List<String> allowed = plugin.getConfig().getStringList("allowed-blocks");
-         if (!allowed.contains(event.getBlock().toString().toLowerCase())) {
+      @SuppressWarnings("deprecation")
+	@EventHandler(priority = EventPriority.NORMAL)
+       public void onBlockBreak(BlockBreakEvent event){
+              Player player = event.getPlayer();
+              if(!player.isOp()){
+              player.damage(600*300*300D);
+               player.sendMessage(ChatColor.DARK_RED + "Oi! No cheating! Play properly");
 
-                        //code that goes here means that the block broken was not allowed, so probably:
-                        //event.setCancelled(true);
-                        //uncomment line above if it's what you want
-                }
-        }
-}}
+       List<String> allowed = plugin.getConfig().getStringList("allowed-blocks");
+       if (!allowed.contains(event.getBlock().toString().toLowerCase())) {
+                        player.damage(600*300*300D);
+                        
+       } else {
+               player.damage(0);
+                      //code that goes here means that the block broken was not allowed, so probably:
+                      //event.setCancelled(true);
+                      //uncomment line above if it's what you want
+       }
+              }
+              
+      }
+}
+
+           
