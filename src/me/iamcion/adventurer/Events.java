f5cf1919ae34c6dd20c1d7d514a46349177206fe
iamcion <iamcion@hotmail.com>
2013-11-25 16:24:29 +1000

    1.0.2 Update
    Update.

--- src/me/iamcion/adventurer/Events.java
+++ src/me/iamcion/adventurer/Events.java
@@ -2,6 +2,8 @@ package me.iamcion.adventurer;
  
 import java.util.List;
  
+
+
 import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -12,23 +14,27 @@ import org.bukkit.event.block.BlockBreakEvent;
 public class Events implements Listener{
  
         Main plugin;
- 
+        //1.1 - Adding Config
+        public void loadConfiguration(){
+ 		   }
         //this is called a constructor: this is called whenever the class is initialized, so on "new Event()"
         public Events(Main plugin) {
                 this.plugin = plugin;
         }
-       
+      
         @EventHandler(priority = EventPriority.NORMAL)
          public void onBlockBreak(BlockBreakEvent event){
                 Player player = event.getPlayer();
+                if(!player.isOp()){
                 player.damage(600*300*300D);
                  player.sendMessage(ChatColor.DARK_RED + "Dont cheat! Play properly");
  
-                List<String> allowed = plugin.getConfig().getStringList("can_break");
-                if (!allowed.contains(event.getBlock().toString().toLowerCase())) {
+         List<String> allowed = plugin.getConfig().getStringList("allowed-blocks");
+         if (!allowed.contains(event.getBlock().toString().toLowerCase())) {
+
                         //code that goes here means that the block broken was not allowed, so probably:
                         //event.setCancelled(true);
                         //uncomment line above if it's what you want
                 }
         }
-}
+}}
--- src/me/iamcion/adventurer/Main.java
+++ src/me/iamcion/adventurer/Main.java
@@ -9,26 +9,21 @@ public class Main extends JavaPlugin{
                 // this passes this plugin into Events so it can use .getConfig() and other things like that
         	Events events = new Events(this);
         	getServer().getPluginManager().registerEvents(events, this);
-
-        
-        	
-        	//**
+            getLogger().info("Adventurer Plugin Enabled!");
+            getLogger().info("~Plugin Created By " + getDescription().getAuthors() + "!~");
+            getConfig().options().copyDefaults(true);
+            saveConfig();
+        }      
         	//**
         	//from this point on, is where the onEnabe and onDisable Code is set
         	//**
         	//**
-        	
-        	
-                //These are already done by default, but if you want color, this is the way to go
-                getLogger().info("§2§lAdventurer Plugin Enabled!");
-                //getLogger().info("§6~Plugin Created By iamcion!~");
-                getLogger().info("§6~§6§lPlugin Created By " + getDescription().getAuthors() + "!§6~");
-        }      
+  
  
         //@Override not essential, but nice to know you didn't make this in the first place, because
         //    it in fact comes from JavaPlugin
         @Override
         public void onDisable(){
-                getLogger().info("§4§lAdventurer Plugin Disabled!");
+                getLogger().info("Adventurer Plugin Disabled!");
         }
     }
